58fdc6c5b7e3d046839244380f68135fbce6cd1b
KolyaVsk <www.nikbang@gmail.com>
2026-09-17 21:13:22 +0300

    [upd] Lesson 11.1

--- Lesson 11/Lesson 11.1.py
+++ Lesson 11/Lesson 11.1.py
@@ -1,24 +1,28 @@
 def rectangle():
     a  = float(input("Ширина: "))
     b = float(input("Высота: "))
-    print (print ("Площадь: %10.f" % (a*b)))
-
-
+    print("Площадь: %2.f" % (a*b))
 
 def triangle():
     a = float(input("Основание: "))
     h = float(input("Высота: "))
     print("Площадь: %2f" % (0.5 * a * h))
 
+def circul():
+    r = float(input("Радиус:"))
+    print ("Площадь: %2.f" %(2 * 3.14159 * r))
 
 
 
 
 
-
-figure = input("1- прямоугольник, 2 - треугольник, 3 - кург")
+figure = input("Введите цифру в соотвесвтии с фиугрой (1- прямоугольник, 2 - треугольник, 3 - кург): ")
 
 if figure == '1':
     rectangle()
-if figure == '2':
+elif figure == '2':
     triangle()
+elif figure == '3':
+    circul()
+else:
+    print("Hет такой фигуры")
