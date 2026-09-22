3baeaa36361e72efefc6769fb74b1e701c7e7d22
Mikalai <www.nikbang@gmail.com>
2026-09-22 11:38:38 +0300

    [upd] lesson 13

---
+++ Lesson 13/Lesson 13.1.py
@@ -0,0 +1,18 @@
+def cylinder():
+    r = float (input())
+    h = float (input())
+    # площадь боковой повехности цилиндра
+    side  = 2 * 3.14 * r  * h
+    # площадь одного основания цилиндра
+    circle = 3.14 * r ** 2
+    # полная площадь цилиндра
+    full = side + 2 * circle
+    return full
+    # в основную ветку передается значение а не переменная
+
+
+print(cylinder())
+
+area = cylinder()
+print(area)
+print(type(area))
