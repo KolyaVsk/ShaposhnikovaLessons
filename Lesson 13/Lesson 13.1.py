ed6939663f2eaf6bcd847cb266319e336aa6d4fc
Mikalai <www.nikbang@gmail.com>
2026-09-24 14:43:27 +0300

    [upd] lesson 13

--- Lesson 13/Lesson 13.1.py
+++ Lesson 13/Lesson 13.1.py
@@ -15,4 +15,4 @@ print(cylinder())
 
 area = cylinder()
 print(area)
-print(type(area))11
+print(type(area))
--- Lesson 13/Lesson 13.2.py
+++ Lesson 13/Lesson 13.2.py
@@ -15,4 +15,4 @@ print ('Площадь основания %.2f' % fCyl)
 
 a = cylinder()
 print(a)
-print(type(a))11
+print(type(a))
